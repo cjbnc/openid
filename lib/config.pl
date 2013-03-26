0a5a8a1087363ec99265cecb4760e896945bc89f
Charles Brabec <cjbrabec@gmail.com>
2013-03-26 13:33:24 -0400

    imported from openid_1_8.tgz

---
+++ lib/config.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#
+# Configuration variables used by the database routines
+#
+
+use strict;
+
+# Define globals for database access
+$main::database_user_id = "DB_USER_ID";
+$main::database_password = "DB_PASSWORD";
+$main::database_name = "DB_NAME";
+$main::database_server = "localhost";
+
+1;
